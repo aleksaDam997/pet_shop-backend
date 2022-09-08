4d71eed40d1acba87d69d8d74ecb44c9bc133708
aleksaDam997 <aleksa.car1997@gmail.com>
2022-09-08 13:09:40 +0200

    cleaning mess

--- src/main/java/com/spring/dto/CartDto.java
+++ src/main/java/com/spring/dto/CartDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.spring.dto;
 
 import java.sql.Date;
@@ -29,35 +28,3 @@ public class CartDto {
 	private Set<PetDto> pets = new HashSet<>();
 	
 }
-=======
-package com.spring.dto;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.HashSet;
-import java.util.Set;
-
-import com.spring.entity.User;
-import com.spring.entity.UserOrder;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CartDto {
-	
-	private Long cartId;
-	
-    private Timestamp createdAt;
-    
-    private User user;
-
-    private UserOrder userOrder;
-    
-	private Set<PetDto> pets = new HashSet<>();
-	
-}
->>>>>>> 1b1b9033059a8b7e6d9387fdb058205b0911bfe6
--- src/main/java/com/spring/dto/QuantityDto.java
+++ src/main/java/com/spring/dto/QuantityDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.spring.dto;
 
 import lombok.AllArgsConstructor;
@@ -11,17 +10,3 @@ import lombok.NoArgsConstructor;
 public class QuantityDto {
 	private int quantity;
 }
-=======
-package com.spring.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class QuantityDto {
-	private int quantity;
-}
->>>>>>> 1b1b9033059a8b7e6d9387fdb058205b0911bfe6
